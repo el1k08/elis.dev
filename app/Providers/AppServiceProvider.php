35276f3ce1d8c529ec5196fa237da27b9ec92b64
Eli Slavutski <eli.slavutski@gmail.com>
2025-11-27 01:36:39 -0330

    fixed ServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use App\Models\User;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use WorkOS\WorkOS; // SDK WorkOS
