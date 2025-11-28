cf19f0ae06fd6cea7c8aec8e329e643375bfe846
Eli Slavutski <eli.slavutski@gmail.com>
2025-11-27 20:53:46 -0330

    added env-debug route

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,15 @@ Route::middleware([
     })->name('dashboard');
 });
 
+Route::get('/env-debug', function () {
+
+    return [
+        'env' => env('WORKOS_API_KEY'),
+        'cfg1' => config('workos.api_key'),
+        'cfg2' => config('services.workos.api_key'),
+    ];
+
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
